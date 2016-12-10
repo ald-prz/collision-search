35fe55cbf67e294040de02e2c6efe64fb82779a9
ElderMayday <galdarsar@gmail.com>
2016-12-10 23:07:54 +0100

    Add example program

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int id, nb_instance, len;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &nb_instance);
+    MPI_Get_processor_name(processor_name, &len);
+
+    cout<<"Hello world! I’m "<<id <<" of "<<nb_instance<<" on "<<processor_name;
+    cout<<endl;
+    MPI_Finalize();
+
+    return 0;
+}
