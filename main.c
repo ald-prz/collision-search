b0de6d6b26af5b24c2d192a605d2aa433ac504b9
ElderMayday <galdarsar@gmail.com>
2016-12-11 01:22:42 +0100

    Find working SHA1 implementation

--- main.c
+++ main.c
@@ -1,36 +1,19 @@
-#include <mpi.h>
-#include <iostream>
-
-using namespace std;
-
-void determine_search_space(int process_id, int process_num, unsigned char *start, unsigned char *end);
+#include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
 
 int main(int argc, char *argv[])
 {
-    int id, nb_instance, len;
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    unsigned char start, end;
+    unsigned char ibuf[] = "compute sha1";
+    unsigned char obuf[20];
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    MPI_Comm_size(MPI_COMM_WORLD, &nb_instance);
-    MPI_Get_processor_name(processor_name, &len);
+    SHA1(ibuf, strlen((const char*)ibuf), obuf);
 
-    determine_search_space(id, nb_instance, &start, &end);
-
-    cout << "process [" << id <<"] from " << +start <<" to "<< +end;
-    cout << endl;
-
-    MPI_Finalize();
+    int i;
+    for (i = 0; i < 20; i++) {
+        printf("%02x ", obuf[i]);
+    }
+    printf("\n");
 
     return 0;
-}
-
-
-void determine_search_space(int process_id, int process_num, unsigned char *start, unsigned char *end)
-{
-    double step = 256.0 / process_num;
-
-    *start = (unsigned char)(process_id * step + 0.5);
-    *end = (unsigned char)((process_id + 1) * step + 0.5) - 1;
 }
