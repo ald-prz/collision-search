a0b6f1360d7edf0aa9182b2c87260d2eb5e9d62d
ElderMayday <galdarsar@gmail.com>
2016-12-14 03:52:18 +0100

    Remove comments and printf

--- main.c
+++ main.c
@@ -16,11 +16,7 @@ void output_collision(const char *filename, int size, unsigned char *word1, unsi
 void rewrite_file(const char *filename);
 void output_runtime(int *runtime, int size);
 
-/*
- * TODO:
- * 1) Disable all printf
- * 2) Try 16 processes once again
-*/
+
 int main(int argc, char *argv[])
 {
     int len;
@@ -31,8 +27,6 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
     MPI_Comm_size(MPI_COMM_WORLD, &process_num);
     MPI_Get_processor_name(processor_name, &len);
-
-    printf("start");
 
     if (argc < 5)
        printf("Arguments are not provided");
@@ -65,10 +59,9 @@ void process_host(int process_id, int process_num, int search_bytes)
     int i;
     MPI_Status status;
 
+    printf("Started\n");
+
     time1 = MPI_Wtime();
-
-    printf("ID:%d;Start host\n", process_id);
-
 
     if (search_bytes == 1)
         target_byte = 254;
@@ -89,8 +82,6 @@ void process_host(int process_id, int process_num, int search_bytes)
     }
     while (byte != target_byte);
 
-    printf("ID:%d;Recieving finish\n", process_id);
-
     // sending finish commands
 
     for (i = 1; i < process_num; i++)
@@ -100,8 +91,6 @@ void process_host(int process_id, int process_num, int search_bytes)
         buff_char[1] = 0;
         MPI_Send(&buff_char, 2, MPI_UNSIGNED_CHAR, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
     }
-
-    printf("ID:%d;Recieving runtime\n", process_id);
 
     // recieving runtimes
 
@@ -113,9 +102,6 @@ void process_host(int process_id, int process_num, int search_bytes)
         runtime[status.MPI_SOURCE] = buff_int[0];
     }
 
-
-    printf("ID:%d;Output\n", process_id);
-
     time2 = MPI_Wtime();
     runtime[0] = (int) (time2 - time1);
 
@@ -125,7 +111,7 @@ void process_host(int process_id, int process_num, int search_bytes)
 
     free(runtime);
 
-    printf("ID:%d;Stop host\n", process_id);
+    printf("Finished\n");
 }
 
 void output_runtime(int *runtime, int size)
@@ -163,13 +149,10 @@ void process_worker(int process_id, int search_bytes, int match_bits)
 
     rewrite_file(filename);
 
-    printf("ID:%d;Start worker\n", process_id);
-
     while (buff_char[0] == 1) // the host orders to continue to work
     {
         MPI_Send(&buff_char, 0, MPI_UNSIGNED_CHAR, 0, 0, MPI_COMM_WORLD);
         MPI_Recv(&buff_char, 2, MPI_UNSIGNED_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        printf("ID:%d;Work:%d\n", process_id, (int)buff_char[1]);
 
         if (buff_char[0] == 1)
         {
@@ -199,12 +182,6 @@ void process_worker(int process_id, int search_bytes, int match_bits)
                     word_end1[i] = 0;
             }
 
-            /*printf("----word1=[");
-            output_word(word1, search_bytes);
-            printf("]-[");
-            output_word(word_end1, search_bytes);
-            printf("]\n");*/
-
             // conduct search on word1 = [byte].x.x...x space
 
             do
@@ -219,35 +196,16 @@ void process_worker(int process_id, int search_bytes, int match_bits)
                 for (i = 0; i < search_bytes; i++)
                     word_end2[i] = 255;
 
-
-                /*printf("--word2=[");
-                output_word(word2, search_bytes);
-                printf("]-[");
-                output_word(word_end2, search_bytes);
-                printf("]\n");*/
-
                 SHA1(word1, search_bytes, digest1);
 
                 do
                 {
                     increment(word2, search_bytes);
 
-                    //output_word(word2, search_bytes);
-                    //printf("\n");                    
-
                     SHA1(word2, search_bytes, digest2);
 
                     if (digest_match(digest1, digest2, match_bits) == 1)
-                        //if (word_match(word1, word2, search_bytes) != 1)
                         output_collision(filename, search_bytes, word1, word2, digest1, digest2);
-
-//                    output_word(word2, search_bytes);
-//                    printf(" ??? ");
-//                    output_word(word_end2, search_bytes);
-//                    printf("\n");
-
-//                    printf("%d\n", match);
-//                    printf("s*8=%d\n", search_bytes * 8);
                 }
                 while (word_match(word2, word_end2, search_bytes) == 0);
 
@@ -265,9 +223,6 @@ void process_worker(int process_id, int search_bytes, int match_bits)
     free(word2);
     free(word_end1);
     free(word_end2);
-
-
-    printf("ID:%d;Stop worker\n", process_id);
 }
 
 
